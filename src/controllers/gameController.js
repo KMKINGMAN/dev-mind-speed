04d5598b5cfd387858f36135e61f33d3d059f3a1
KMKINGMAN <kingkurkar@gmail.com>
2025-02-22 15:24:40 +0300

    Fix variable declaration for game creation in game controller

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -22,13 +22,14 @@ async (req, res) => {
         return res.status(400).json({ error: "Failed to generate equation" });
     }
 
+    let game;
     try {
-        const game = await Game.create({ name, difficulty, question, answer })
+        game = await Game.create({ name, difficulty, question, answer })
     }
     catch (error) {
         return res.status(500).json({ error: "Failed to create game" });
     }
-    
+
     res.json({
         message: `Hello ${name}, find your submit API URL below`,
         submit_url: `/game/${game._id}/submit`,
